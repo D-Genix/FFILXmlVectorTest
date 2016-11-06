bd4ed62c7295c72f4361e537e6cfed641ee9b121
federicomaccaroni <federicomaccaroni@hotmail.com>
2016-11-06 20:30:18 -0300

    Add project files.

---
+++ FFILXmlVector/FFILXmlVector.Droid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using FFImageLoading;
+using FFImageLoading.Views;
+
+namespace FFILXmlVector.Droid
+{
+    [Activity (Label = "FFILXmlVector.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme")]
+	public class MainActivity : AppCompatActivity
+	{
+		int count = 1;
+
+        ImageViewAsync imgXml, imgPng;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            this.imgPng = this.FindViewById<ImageViewAsync>(Resource.Id.img_common_png_placeholder);
+            this.imgXml = this.FindViewById<ImageViewAsync>(Resource.Id.img_vector_drawable_placeholder);
+
+            // Loads the image with png placeholders (works)
+            ImageService.Instance.LoadUrl("This should load the error placeholder")
+                .LoadingPlaceholder("@drawable/Icon", FFImageLoading.Work.ImageSource.CompiledResource)
+                .ErrorPlaceholder("@drawable/Icon", FFImageLoading.Work.ImageSource.CompiledResource)
+                .Into(this.imgPng);
+
+            // Loads the image with xml placeholders (doesn't work)
+            ImageService.Instance.LoadUrl("This should load the error placeholder")
+                .LoadingPlaceholder("@drawable/freesample", FFImageLoading.Work.ImageSource.CompiledResource)
+                .ErrorPlaceholder("@drawable/freesample", FFImageLoading.Work.ImageSource.CompiledResource)
+                .Into(this.imgXml);
+        }
+	}
+}
---
+++ FFILXmlVector/FFILXmlVector/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FFILXmlVector
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
